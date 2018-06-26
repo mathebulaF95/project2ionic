dfb11c53b0a0d67e552438a104763f71a9af5961
mathebulaF95 <mathebulaF95@gmail.com>
2018-06-26 13:52:55 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  scoreA:number;
+  scoreB:number;
+
   constructor(public navCtrl: NavController) {
 
+ this.scoreA = 14;
+ this.scoreB = 25;
+
+  }
+
+  Click(num:number){
+    if(num == 3){
+        this.scoreA += 3;
+    }
+    else if ( num == 2){
+       this.scoreA += 2;
+      }
+      else{
+      this.scoreA += 1;
+    }
+    
+  }
+
+  ClickB(num:number){
+    if(num == 3){
+        this.scoreB += 3;
+    }
+    else if ( num== 2){
+       this.scoreB += 2;
+     
+    }else{
+      this.scoreB += 1;
+    }
+    
+  }
+
+  OnClick(){
+    this.scoreA = 0;
+    this.scoreB = 0;
   }
 
 }
